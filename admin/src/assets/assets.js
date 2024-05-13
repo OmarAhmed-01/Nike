62e77dd4b198cbcdaba1253b38d0320b3dca4e7e
Omar Ahmed <omarahmed19991@outlook.com>
2024-05-13 22:06:11 +0300

    -Add a side menu for mobiles containing a page links

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,11 @@
 import logo from './logo.png'
 import upload_sign from './upload.png'
+import burgerMenu from './burger.png'
+import closeMenu from './close.png'
 
 export const assets = {
     logo,
-    upload_sign
+    upload_sign,
+    burgerMenu,
+    closeMenu
 }
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,30 +1,79 @@
-import React from 'react'
-import './navbar.css'
-import { assets } from '../assets/assets.js'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "./navbar.css";
+import { assets } from "../assets/assets.js";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div className='navbar-container'>
-        <div className="logo">
-            <Link to='/'>
-                <img src={assets.logo} alt="Logo" />
-                <h1>Admin Panel</h1>
-            </Link>    
-        </div>
-        <div className='items'>
-            <ul className="navbar-menu">
-                <Link to='/add'>Add</Link>
-                <Link to='/list'>List</Link>
-                <Link to='/orders'>Orders</Link>
-            </ul>
-        </div>
-        <div className='admin-details'>
-            <img src='#' alt="" />
-            <h1>Name</h1>
-        </div>
-    </div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
 
-export default Navbar
+  const handleMenuChange = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    setMenuOpen(false)
+  }, [location.pathname]);
+
+  useEffect(() => {
+    console.log(menuOpen)
+  }, [menuOpen]);
+
+  return (
+    <div className="navbar-container">
+      <div className="logo">
+        <Link to="/">
+          <img src={assets.logo} alt="Logo" />
+          <h1>Admin Panel</h1>
+        </Link>
+      </div>
+      <div className="items">
+        <ul className="navbar-menu">
+          <Link to="/add">Add</Link>
+          <Link to="/list">List</Link>
+          <Link to="/orders">Orders</Link>
+        </ul>
+      </div>
+      <div className="admin-details">
+        <h1>Admin</h1>
+      </div>
+      <div className={menuOpen ? "menuClosed":"open-menu"}>
+        <img src={assets.burgerMenu} onClick={handleMenuChange} alt="" />
+      </div>
+      <div className={menuOpen ? "menu":"menuClosed"}>
+        <img
+          src={assets.closeMenu}
+          onClick={handleMenuChange}
+          alt=""
+        />
+        <div className={menuOpen ? "menu-list" : "menuClosed"}>
+          <div className="menu-items">
+            <ul className="navbar-menu-items">
+              <Link to="/add">Add</Link>
+              <Link to="/list">List</Link>
+              <Link to="/orders">Orders</Link>
+            </ul>
+          </div>
+          <div className="menu-admin-details">
+            <h1>Admin</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+{
+  /* <div className="menu">
+        <div className="burger-menu">
+          <img
+            src={menuOpen ? assets.closeMenu : assets.burgerMenu}
+            onClick={() => handleMenuChange(!menuOpen)}
+            alt=""
+          />
+        </div>
+        
+      </div> */
+}
